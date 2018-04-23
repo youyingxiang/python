c61a883d4f5c8d35cd58998102aa6b9f9581873d
youxingxiang <1365831278@qq.com>
2018-04-23 21:43:59 +0800

    参考php验证码插件

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,2 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint,request
+from utils.captche.captche import Captcha
 bp = Blueprint("common",__name__,url_prefix='/common')
+
+
+
+@bp.route('/')
+def test():
+    c = Captcha()
+    code = request.args.get('code')
+    print(c.check(code))
+    return '123'
+    # return c.entry()
+    # print(c.imageW)
+    # return '123'
+@bp.route('/index/')
+def test2():
+    c = Captcha()
+    return c.entry()
